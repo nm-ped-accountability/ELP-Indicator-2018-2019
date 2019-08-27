1760a416bfc35e15d932f175132e57235b5232a5
yaoyunhz <yaoyunhz@gmail.com>
2019-08-27 14:51:23 -0600

    Update ELP Indicator Calculations.R

--- ELP Indicator Calculations.R
+++ ELP Indicator Calculations.R
@@ -1,3 +1,39 @@
-### ELP Indicator 2018-2019
+### ELP Indicator 201-2018
+
+
+# Load data ---------------------------------------------------------------
 
 library(tidyverse)
+
+rm(list = ls())
+
+year0_raw <- read.csv("NM_Summative_Stud_file 2017.csv", 
+                  header = TRUE, stringsAsFactors = FALSE)
+
+year1_raw <- read.csv("ACCESS for ELLs 2017-2018 Complete Cases 2019-08-12.csv",
+                  header = TRUE, stringsAsFactors = FALSE)
+
+year2_raw <- read.csv("ACCESS for ELLs 2018-2019 Complete Cases 2019-07-18.csv",
+                  header = TRUE, stringsAsFactors = FALSE)
+
+
+# Match year0 and year1 ---------------------------------------------------
+
+year0 <- year0_raw %>%
+    select(District.Number, School.Number, State.Student.ID, 
+           Grade, Composite..Overall..Proficiency.Level) %>%
+    filter(!is.na(Composite..Overall..Proficiency.Level)) %>%
+    mutate(distcode = as.numeric(gsub("NM", "", District.Number)),
+           schcode = School.Number,
+           schnumb = distcode * 1000 + schcode,
+           stid = State.Student.ID,
+           grade = Grade,
+           pl = Composite..Overall..Proficiency.Level,
+           sy = 2017) %>%
+    select(distcode, schcode, schnumb, stid, grade, pl, sy)
+
+head(year0)
+
+names(year1_raw)
+year1 <- year1_raw %>%
+    select(test_schnumb, distcode, schcode, stid, grade)
