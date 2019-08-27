6819204efd3b6554ac618677d1d0380fb5e40867
yaoyunhz <yaoyunhz@gmail.com>
2019-08-27 16:45:10 -0600

    Update ELP Indicator Calculations.R

--- ELP Indicator Calculations.R
+++ ELP Indicator Calculations.R
@@ -10,7 +10,7 @@ rm(list = ls())
 year0_raw <- read.csv("NM_Summative_Stud_file 2017.csv", 
                   header = TRUE, stringsAsFactors = FALSE)
 
-year1_raw <- read.csv("ACCESS for ELLs 2017-2018 Complete Cases 2019-08-12.csv",
+year1_raw <- read.csv("ACCESS for ELLs 2017-2018 Complete Cases 2019-08-27.csv",
                   header = TRUE, stringsAsFactors = FALSE)
 
 year2_raw <- read.csv("ACCESS for ELLs 2018-2019 Complete Cases 2019-07-18.csv",
@@ -18,7 +18,6 @@ year2_raw <- read.csv("ACCESS for ELLs 2018-2019 Complete Cases 2019-07-18.csv",
 
 
 # Match year0 and year1 ---------------------------------------------------
-
 year0 <- year0_raw %>%
     select(District.Number, School.Number, State.Student.ID, 
            Grade, Composite..Overall..Proficiency.Level) %>%
@@ -34,6 +33,14 @@ year0 <- year0_raw %>%
 
 head(year0)
 
-names(year1_raw)
 year1 <- year1_raw %>%
-    select(test_schnumb, distcode, schcode, stid, grade)
+    select(test_schnumb, distcode, schcode, stid, grade, PL_composite) %>%
+    mutate(schnumb = test_schnumb,
+           pl = PL_composite,
+           sy = 2018) %>%
+    select(distcode, schcode, schnumb, stid, grade, pl, sy)
+
+head(year1)
+
+year01 <- left_join(year1, year0, by = "stid")
+head(year01)
